31fd942421a668215a68fa053055acc042a7cf33
ebrima <barry99ebrima@gmail.com>
2026-06-07 16:07:02 +0000

    fixed business intelligence route in business detail page

--- build.sh
+++ build.sh
@@ -6,6 +6,9 @@ echo "Building GAMBIH Backend..."
 # Install dependencies
 pip install -r requirements.txt
 
+# check for migrations
+python manage.py makemigrations
+
 # Run migrations
 python manage.py migrate
 
